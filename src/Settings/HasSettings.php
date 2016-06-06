594dc99d36be5b605f57bfbf690e9933452adfc9
Zach Leigh <zachleigh@fastmail.jp>
2016-06-06 14:26:19 +0900

    Improve dynamic class resolution.

--- src/Settings/HasSettings.php
+++ src/Settings/HasSettings.php
@@ -3,10 +3,18 @@
 namespace LaravelPropertyBag\Settings;
 
 use LaravelPropertyBag\UserSettings\UserSettings;
+use \Illuminate\Console\AppNamespaceDetectorTrait;
 use LaravelPropertyBag\UserSettings\UserPropertyBag;
 
 trait HasSettings
 {
+    use AppNamespaceDetectorTrait;
+
+    /**
+     * Saved settings.
+     *
+     * @var Collection
+     */
     protected $settingsInstance = null;
 
     /**
@@ -66,10 +74,18 @@ trait HasSettings
      */
     protected function getSettingsClass()
     {
+        $settingsName = $this->getSettingsName();
+
+        $appNamespace = $this->getAppNamespace();
+
+        $classNamespace = $appNamespace.$settingsName.'\\'.$settingsName;
+
         if (isset($this->settingsClass)) {
             return $this->settingsClass;
+        } else if (class_exists($classNamespace)) {
+            return $classNamespace;
         }
-
+        
         return UserSettings::class;
     }
 
@@ -80,10 +96,52 @@ trait HasSettings
      */
     public function getPropertyBagClass()
     {
+        $settingsName = $this->getSettingsName();
+
+        $propertyBagName = $this->getPropertyBagName();
+
+        $appNamespace = $this->getAppNamespace();
+
+        $classNamespace = $appNamespace.$settingsName.'\\'.$propertyBagName;
+
         if (isset($this->propertyBagClass)) {
             return $this->propertyBagClass;
+        } else if (class_exists($classNamespace)) {
+            return $classNamespace;
         }
 
         return UserPropertyBag::class;
+    }
+
+    /**
+     * Get name for settings.
+     *
+     * @return string
+     */
+    protected function getSettingsName()
+    {
+        return $this->getShortClassName().'Settings';
+    }
+
+    /**
+     * Get name for property bag.
+     *
+     * @return string
+     */
+    protected function getPropertyBagName()
+    {
+        return $this->getShortClassName().'PropertyBag';
+    }
+
+    /**
+     * Get the short name of the model.
+     *
+     * @return string
+     */
+    protected function getShortClassName()
+    {
+        $reflection = new \ReflectionClass($this);
+
+        return $reflection->getShortName();
     }
 }
